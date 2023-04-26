292779465a561921d95f597220223792cc0c54d1
giannisrig <giannisrig93@gmail.com>
2023-04-26 17:43:20 +0300

    Update Demo components & Header & Footer
    Also added some images and changes in the tailwind config

--- src/components/common/footer/subFooter.js
+++ src/components/common/footer/subFooter.js
@@ -3,7 +3,7 @@ export default function SubFooter() {
     <div className="w-full border-t border-bleached font-secondary">
       <div className="container">
         <div className="flex w-full justify-between py-20px lg:py-30px">
-          <div className="text-sm">Copyright</div>
+          <div className="text-sm">© 2023, All Rights Reserved</div>
           <div className="text-sm">
             <span className="opacity-70">Maybe some menu items here</span>
           </div>
